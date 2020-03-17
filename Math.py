5062296ecca99b5b167f0639324fe5fbafc0d98f
Tejas Ravindra Dhawale <tejasdhawale89@gmail.com>
2020-03-17 19:49:06 +0100

    Changed the value

--- Math.py
+++ Math.py
@@ -1,8 +1,8 @@
 import math as m
 # here I have used alias for math library.You can call the function
 #without alias by math.ceil(2.6)
-print(m.ceil(2.8))
-print(m.floor(2.8))
+print(m.ceil(3.8))
+print(m.floor(3.8))
 print(m.pi) #value of Pi
 print(m.e)  #value of Epsilon
 print(m.pow(3,4)) # it is 3 to power of 4
