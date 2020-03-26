e982f2e1c42c341ef336739d33d7ad94eb21a09b
Tejas Ravindra Dhawale <tejasdhawale89@gmail.com>
2020-03-26 12:18:09 +0100

    Loop changes

--- while_loop.py
+++ while_loop.py
@@ -4,3 +4,4 @@ while i <= 6:
     i += 1
 else:
     print("End of while loop after", i, "iterations")
+# https: // github.com/Kunena/Kunena-Forum/wiki/Create-a-new-branch-with-git-and-manage-branches
