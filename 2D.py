c852e2f90bfaa0d8a0b19b7f1e28145ecf8c40cf
Tejas Ravindra Dhawale <tejasdhawale89@gmail.com>
2020-03-28 18:29:31 +0100

    2D

---
+++ 2D.py
@@ -0,0 +1,14 @@
+from numpy import *
+
+arr1 = array([[1, 22, 3],
+              [5, 6, 7]
+
+              ])
+
+print(arr1)
+print("The type of array is", arr1.dtype)
+print("The size of array is", arr1.size)
+print("The dimension of array are:", arr1.ndim)
+print(arr1.shape)
+arr1.sort()
+print(arr1)
